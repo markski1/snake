9240f863916ab51e99d795703ce72dc0f64af19f
Markski <me@markski.ar>
2023-05-23 19:57:21 -0300

    remove unused function

--- platform_specific/win-platform.h
+++ platform_specific/win-platform.h
@@ -10,23 +10,6 @@ void set_scrsize() {
     printf("\033]0; SNEPK! \007");
 }
 
-// wait a given amount of time, less as the snake becomes longer,
-// and check for keyboard input afterwards. update the snake's behaviour if required.
-void wait_and_key()
-{
-	int wait_amount = BASE_WAIT - (snk.len * 2);
-
-    // if moving vertically slow things down, since vertical characters are higher
-    if (snk.dir == MOVE_UP || snk.dir == MOVE_DOWN)
-        wait_amount *= 1.3;
-
-    if (wait_amount < 32) wait_amount == 32;
-    else if (wait_amount < 50) wait_amount * 1.07;
-    else if (wait_amount < 75) wait_amount * 1.15;
-    
-	Sleep(wait_amount);
-}
-
 void ms_sleep(int ms) {
     Sleep(ms);
 }
